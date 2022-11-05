480dd3232361e1231cef1487618e1ff9782f534d
Pavel Bandaronak <p_bandaronak@locport.io>
2022-11-05 18:07:16 +0300

    fix: remove extra code from 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -8,23 +8,3 @@ stream.on('data', (data) => {
   content += data;
 });
 stream.on('end', () => console.log(content));
-
-
-
-// fs.readFile(path.resolve(__dirname, 'text.txt'), (err, data) => {
-//   if (err) throw err;
-//   const content = data.toString();
-//   console.log(content);
-// });
-
-
-// // const data = new Promise((res, rej) => {
-// //   fs.readFile('./text.txt', (err, buffer) => {
-// //     res(buffer.toString());
-// //   })
-// // });
-//
-// (async function(){
-//   const data =  await fs.promises.readFile('./text.txt');
-//   console.log(data.toString());
-// })();
