ee40f06135468cc666fd06944b5bd576241ff6af
Pavel Bandaronak <p_bandaronak@locport.io>
2022-11-05 15:16:35 +0300

    feat: add solution for task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,31 @@
+const {readdir, mkdir, copyFile, rm} = require('fs/promises');
+const path = require('path');
 
+const SOURCE_DIRECTORY_NAME = 'files';
+const TARGET_DIRECTORY_NAME = 'files-copy';
+
+const cleanOrCreateTargetDir = async () => {
+  const dirPath = path.resolve(__dirname, TARGET_DIRECTORY_NAME);
+  const targetDir = await readdir(dirPath, {withFileTypes: true})
+    .then(files => {
+      const tasks = [];
+      for (const file of files) {
+        const filePath = path.resolve(__dirname, TARGET_DIRECTORY_NAME, file.name);
+        tasks.push(rm(filePath));
+      }
+      return Promise.all(tasks);
+    }, error => {
+      return mkdir(dirPath, {recursive: true});
+    });
+  return dirPath;
+};
+
+(async () => {
+  const targetDir = await cleanOrCreateTargetDir();
+  const files = await readdir(path.resolve(__dirname, SOURCE_DIRECTORY_NAME), {withFileTypes: true });
+  for (const file of files) {
+    const sourcePath = path.resolve(__dirname, SOURCE_DIRECTORY_NAME, file.name);
+    const targetPath = path.resolve(__dirname, TARGET_DIRECTORY_NAME, file.name);
+    const copy = await copyFile(sourcePath, targetPath);
+  }
+})();
