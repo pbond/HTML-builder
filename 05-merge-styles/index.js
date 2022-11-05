e6468a6359a2910908fed12e34de135c35a5791d
Pavel Bandaronak <p_bandaronak@locport.io>
2022-11-05 16:06:40 +0300

    feat: add solution for task 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,28 @@
+const {readdir, appendFile} = require('fs/promises');
+const path = require('path');
+const fs = require("fs");
+
+const SOURCE_DIRECTORY_PATH = path.resolve(__dirname, 'styles');
+const TARGET_DIRECTORY_PATH = path.resolve(__dirname, 'project-dist');
+
+const getFileContent = async (file) => {
+  const stream = fs.createReadStream(path.resolve(SOURCE_DIRECTORY_PATH, file.name));
+  return new Promise((resolve, reject) => {
+    let content = '';
+    stream.on('data', (data) => content += data);
+    stream.on('end', () => {
+      stream.close();
+      resolve(content);
+    });
+  });
+};
+
+(async () => {
+  const files = await readdir(SOURCE_DIRECTORY_PATH, {withFileTypes: true});
+  for (const file of files) {
+    if (file.name.indexOf('css') > 1) { //1 symbol as min filename and 1 symbol for dot
+      const content = await getFileContent(file);
+      const result = appendFile(path.resolve(TARGET_DIRECTORY_PATH, 'bundle.css'), content);
+    }
+  }
+})();
