d7e789e36f024f5f2587dd962ec5319bfc6175f7
Pavel Bandaronak <p_bandaronak@locport.io>
2022-11-05 16:07:04 +0300

    fix: remake solution for task 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,25 +1,20 @@
 const fs = require('fs');
 const path = require("path");
-const readline = require('node:readline');
 
-// const stream = fs.createWriteStream(path.resolve(__dirname, 'input.txt'));
-let content = '';
+const stream = fs.createWriteStream(path.resolve(__dirname, 'input.txt'));
 
-const reader = readline.createInterface(process.stdin);
-reader.on('line', (data) => {
+process.stdin.on('data', data => {
   if (data === 'exit') {
     streamClose();
     return;
   }
-  content += data;
+  stream.write(data);
 });
 
 const streamClose = () => {
-  fs.writeFile(path.resolve(__dirname, 'input.txt'), content, (err) => {
-    console.log('The file has been saved!');
-    process.exit();
-  });
+  console.log('The file has been saved!');
+  process.exit();
 };
 
 process.on('SIGINT', streamClose);
-console.log('Hello! Please, input something in the console:')
+console.log('Hello! Please, input something in the console:');
